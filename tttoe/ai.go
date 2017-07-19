c0fdaddd825b7588091c89f88df3a748357af310
Brice Colucci <brice.colucci@gmail.com>
2017-07-19 17:21:52 +0200

    wip on ai first version

--- tttoe/ai.go
+++ tttoe/ai.go
@@ -1,5 +1,11 @@
 package tttoe
 
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
 type AI struct {
 	Symbol string
 }
@@ -8,9 +14,33 @@ func NewAI(symbol string) *AI {
 	return &AI{Symbol: symbol}
 }
 
-func (ai *AI) bestPlay(stage *Stage) (int, int) {
-	//TODO
-	return 0, 0
+//TODO Fix it
+func (ai *AI) playSomeWhere(stage *Stage) (int, int) {
+	rows, cols, _ := stage.Split()
+	emptyCells := [][]int{}
+	for i := 0; i < 3; i += 1 {
+		rowEmptyCellIdx := strings.Index(rows[i], Symbols[Empty])
+		if rowEmptyCellIdx > -1 {
+			x := rowEmptyCellIdx % 3
+			if strings.Contains(rows[i], ai.Symbol) {
+				return i, x
+			}
+			fmt.Printf("Empty cell %d,%d\n", i, x)
+			emptyCells = append(emptyCells, []int{i, x})
+		}
+		colEmptyCellIdx := strings.Index(cols[i], Symbols[Empty])
+		if colEmptyCellIdx > -1 {
+			y := colEmptyCellIdx % 3
+			if strings.Contains(cols[i], ai.Symbol) {
+				return y, i
+			}
+			fmt.Printf("Empty cell %d,%d\n", y, i)
+			emptyCells = append(emptyCells, []int{y, i})
+		}
+	}
+	//TODO check diagonals
+	randCell := emptyCells[rand.Intn(len(emptyCells))]
+	return randCell[0], randCell[1]
 }
 
 func (ai *AI) NextPlay(stage *Stage) (int, int) {
@@ -20,5 +50,5 @@ func (ai *AI) NextPlay(stage *Stage) (int, int) {
 	if canWin, y, x := stage.OneStepToWin(Symbols[Player1]); canWin {
 		return y, x
 	}
-	return ai.bestPlay(stage)
+	return ai.playSomeWhere(stage)
 }
