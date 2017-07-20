eedda3c694b49ba46f65332638a0f649d392de0f
Brice Colucci <brice.colucci@gmail.com>
2017-07-20 10:04:51 +0200

    try to fix empty cells detection loop

--- tttoe/ai.go
+++ tttoe/ai.go
@@ -3,6 +3,7 @@ package tttoe
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"strings"
 )
 
@@ -14,33 +15,48 @@ func NewAI(symbol string) *AI {
 	return &AI{Symbol: symbol}
 }
 
-//TODO Fix it
 func (ai *AI) playSomeWhere(stage *Stage) (int, int) {
 	rows, cols, _ := stage.Split()
-	emptyCells := [][]int{}
+	fmt.Println(rows)
+	fmt.Println(cols)
+	emptyCells := []string{}
+	pushEmptyCell := func(x, y int) {
+		cell := strconv.Itoa(x) + "" + strconv.Itoa(y)
+		found := false
+		for _, c := range emptyCells {
+			if cell == c {
+				found = true
+				break
+			}
+		}
+		if !found {
+			emptyCells = append(emptyCells, cell)
+			fmt.Printf("Empty cell (%d,%d)\n", x, y)
+		}
+	}
+	//TODO fix this loop, we don't check all cells
 	for i := 0; i < 3; i += 1 {
 		x := strings.Index(rows[i], Symbols[Empty])
 		if x > -1 {
-			fmt.Printf("Found a row with an empty cell (->%d,%d)\n", x, i)
 			if strings.Contains(rows[i], ai.Symbol) {
 				return x, i
 			}
-			fmt.Printf("Empty cell (%d,%d)\n", x, i)
-			emptyCells = append(emptyCells, []int{x, i})
+			pushEmptyCell(x, i)
 		}
 		y := strings.Index(cols[i], Symbols[Empty])
 		if y > -1 {
-			fmt.Printf("Found a row with an empty cell (%d,->%d)\n", i, y)
 			if strings.Contains(cols[i], ai.Symbol) {
 				return i, y
 			}
-			fmt.Printf("Empty cell (%d,%d)\n", i, y)
-			emptyCells = append(emptyCells, []int{i, y})
+			pushEmptyCell(i, y)
 		}
 	}
 	//TODO check diagonals
 	randCell := emptyCells[rand.Intn(len(emptyCells)-1)]
-	return randCell[0], randCell[1]
+	cellCoordinates := strings.Split(randCell, "")
+	x, _ := strconv.Atoi(cellCoordinates[0])
+	y, _ := strconv.Atoi(cellCoordinates[1])
+	return x, y
 }
 
 func (ai *AI) NextPlay(stage *Stage) (int, int) {
